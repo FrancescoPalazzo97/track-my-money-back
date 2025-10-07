2572906e99c200c588579b72086a632ed6f52804
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-10-07 13:10:15 +0200

    Minor changes

--- src/lib/utility.ts
+++ src/lib/utility.ts
@@ -32,6 +32,7 @@ export async function convertTransaction(transaction: TTransaction, baseCurrency
         if (!exchangeRate) {
             throw new Error(`Exchange rate per la data ${dayjs(transaction.transactionDate).format('DD-MM-YYYY')} non trovato`)
         };
+
         const targetDate = dayjs(transaction.transactionDate).subtract(1, "day").format('YYYY-MM-DD');
         const diff = dayjs(exchangeRate.meta.last_updated_at).diff(dayjs(`${targetDate}T23:59:59Z`), 'day');
         if (diff < 0) {
@@ -41,11 +42,11 @@ export async function convertTransaction(transaction: TTransaction, baseCurrency
                     $lte: transaction.transactionDate
                 }
             }).sort({ 'meta.last_updated_at': -1 });
+            if (!exchangeRate) {
+                throw new Error(`Exchange rate per la data ${dayjs(transaction.transactionDate).format('DD-MM-YYYY')} non trovato`)
+            };
         }
 
-        if (!exchangeRate) {
-            throw new Error(`Exchange rate per la data ${dayjs(transaction.transactionDate).format('DD-MM-YYYY')} non trovato`)
-        };
 
         const currencyData = exchangeRate.data.get(baseCurrency)
             ?? (() => { throw new Error(`La valuta ${baseCurrency} non è stata trovata!`) })();
